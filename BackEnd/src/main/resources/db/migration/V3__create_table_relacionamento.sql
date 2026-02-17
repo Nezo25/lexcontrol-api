a9e90e5ed17751b5d9ad63d90341bb296081f8f5
Pedro Stein <65890570+Pedro6Stein@users.noreply.github.com>
2026-02-17 02:06:26 -0300

    chore: remove backend from frontend branch

--- BackEnd/src/main/resources/db/migration/V3__create_table_relacionamento.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE cliente_advogado (
-    cliente_id BIGINT NOT NULL,
-    advogado_id BIGINT NOT NULL,
-    PRIMARY KEY (cliente_id, advogado_id),
-    CONSTRAINT fk_link_cliente FOREIGN KEY (cliente_id) REFERENCES clientes(id),
-    CONSTRAINT fk_link_advogado FOREIGN KEY (advogado_id) REFERENCES advogados(id_advogado)
-);
